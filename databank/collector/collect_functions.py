dc217bc03ef95ddaca66b8ef379206195d43d34f
danisafta <saftamailbox@gmail.com>
2021-04-05 19:41:03 +0300

    WIP

--- databank/collector/collect_functions.py
+++ databank/collector/collect_functions.py
@@ -1,8 +1,32 @@
+import time
+import board
+import adafruit_dht
+ 
+dhtDevice = adafruit_dht.DHT11(board.D4, use_pulseio=False)
+ 
 def temperature_value():
     print("[LOG] Collecting TEMPERATURE data")
+    temperature_c = dhtDevice.temperature
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
 
 def humidity_value():
     print("[LOG] Collecting HUMIDITY data")
+    humidity = dhtDevice.humidity
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
 
 def pressure_value():
     print("[LOG] Collecting PRESSURE data")
