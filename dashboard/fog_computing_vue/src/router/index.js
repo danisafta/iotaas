fcef11a8d3839c3f38c8bad1f535761acf171312
Laura Iscrulescu <laura.iscrulescu@yahoo.com>
2021-06-10 16:26:42 +0300

    Added SignUp page

--- dashboard/fog_computing_vue/src/router/index.js
+++ dashboard/fog_computing_vue/src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import Sensor from '../views/Sensor.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
+import SignUp from '../views/SignUp.vue'
 
 const routes = [
   {
@@ -17,6 +18,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
   },
   {
     path: '/search',
