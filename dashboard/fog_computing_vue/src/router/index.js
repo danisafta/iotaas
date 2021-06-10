b1c85a857f35641fe418c78892c44ae38ca4f1b8
Laura Iscrulescu <laura.iscrulescu@yahoo.com>
2021-06-10 18:18:20 +0300

    Added MyAccount page

--- dashboard/fog_computing_vue/src/router/index.js
+++ dashboard/fog_computing_vue/src/router/index.js
@@ -1,16 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
 import Home from '../views/Home.vue'
 import Sensor from '../views/Sensor.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
+import MyAccount from '../views/MyAccount.vue'
+
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+
   },
   {
     path: '/about',
@@ -29,6 +33,14 @@ const routes = [
     path: '/log-in',
     name: 'LogIn',
     component: LogIn
+  },
+  {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
   },
   {
     path: '/search',
@@ -52,6 +64,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'LogIn', query: { to: to.path } });
+  } else {
+    next()
+  }
 })
 
 export default router
