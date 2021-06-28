5ef8c1eff210c10bcb332bca344543caa4479b9f
danisafta <saftamailbox@gmail.com>
2021-06-28 21:35:01 +0300

    Ansible playbook and code

--- ansible/roles/things/files/relays.py
+++ ansible/roles/things/files/relays.py
@@ -2,11 +2,14 @@ from flask import Flask, jsonify, request
 import RPi.GPIO as GPIO
 import socket
 from flask_cors import CORS
+relays_map = {}
 
 
 def on(pin):
     try:
         GPIO.output(pin, GPIO.HIGH)
+        pin = str(pin)
+        relays_map[pin] = True
     except:
         print("Error when trying to turn on")
 
@@ -14,6 +17,8 @@ def on(pin):
 def off(pin):
     try:
         GPIO.output(pin, GPIO.LOW)
+        pin = str(pin)
+        relays_map[pin] = False
     except:
         print("Error when trying to turn off")
 
@@ -22,7 +27,6 @@ URL = "http://k8spi.go.ro:8081/event"
 node_name = socket.gethostname()
 node_name = node_name.upper()
 
-
 app = Flask(__name__)
 CORS(app)
 
@@ -30,18 +34,21 @@ CORS(app)
 @app.route('/state/<id>', methods=["GET"])
 def get_relay(id):
     try:
-        id = int(id)
-        pin_state = GPIO.input(id)
+        pin_state = relays_map.get(id)
+        if pin_state is None:
+            pin_state = False
         return "State is {} for relay_id = {}".format(pin_state, id)
     except:
         return "State is off for relay_id = {}".format(id)
 
 
-@app.route('/relay/<relay_id>', methods=["POST"])
-def relay(id):
+@app.route('/relay', methods=["POST"])
+def relay():
     '''id == gpio pin number'''
     data = request.get_json()
     state = data.get('state')
+    id = data.get('id')
+    id = int(id)
 
     # GPIO setup
     GPIO.setmode(GPIO.BCM)
@@ -56,5 +63,5 @@ def relay(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5010)
+    app.run(host="0.0.0.0", port=5010)
     GPIO.cleanup()
